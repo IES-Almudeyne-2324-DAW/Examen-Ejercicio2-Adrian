74aa37710c422903e975dfa50d9d8ce2b5fb6b06
adrimartinez750 <amarseg372@iesalmudeyne.es>
2023-12-12 14:22:43 +0100

    subida java1

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,5 +1,5 @@
 public class HolaMundo {
     public static void main (String[] args){
-        System.out.println("Hola Mundo");//Hola mundo
+        System.out.println("Hola Mundo y atodas las personas que estan viendo esto");//Hola mundo
     }
 }
